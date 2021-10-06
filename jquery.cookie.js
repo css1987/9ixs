f8560cb7dd049a291fc86e6ee2895007b3f63603
lewenxs <43432664+lewenxs@users.noreply.github.com>
2021-10-06 23:02:23 +0800

    Create jquery.cookie.js

---
+++ jquery.cookie.js
@@ -0,0 +1,44 @@
+(function($, j, k) {
+ var m = /\+/g;
+
+ function raw(s) {
+  return s
+ }
+
+ function decoded(s) {
+  return decodeURIComponent(s.replace(m, ' '))
+ }
+ var n = $.cookie = function(a, b, c) {
+  if (b !== k) {
+   c = $.extend({}, n.defaults, c);
+   if (b === null) {
+    c.expires = -1
+   }
+   if (typeof c.expires === 'number') {
+    var d = c.expires,
+    t = c.expires = new Date();
+    t.setDate(t.getDate() + d)
+   }
+   b = n.json ? JSON.stringify(b) : String(b);
+   return (j.cookie = [encodeURIComponent(a), '=', n.raw ? b : encodeURIComponent(b), c.expires ? '; expires=' + c.expires.toUTCString() : '', c.path ? '; path=' + c.path : '', c.domain ? '; domain=' + c.domain : '', c.secure ? '; secure' : ''].join(''))
+  }
+  var e = n.raw ? raw : decoded;
+  var f = j.cookie.split('; ');
+  for (var i = 0, l = f.length; i < l; i++) {
+   var g = f[i].split('=');
+   if (e(g.shift()) === a) {
+    var h = e(g.join('='));
+    return n.json ? JSON.parse(h) : h
+   }
+  }
+  return null
+ };
+ n.defaults = {};
+ $.removeCookie = function(a, b) {
+  if ($.cookie(a) !== null) {
+   $.cookie(a, null, b);
+   return true
+  }
+  return false
+ }
+})(jQuery, document);
